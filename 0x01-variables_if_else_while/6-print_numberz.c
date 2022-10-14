1f43474dd96d6e45c7516c332499370bed1a78b7
Aroh Abuchi <arohabuchi@gmail.com>
2022-10-14 08:59:40 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print from 9 to 0
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n >= '9' n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
