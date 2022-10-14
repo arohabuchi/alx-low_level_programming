1762d89919da22454ec90972d16c8fe531ce8524
Aroh Abuchi <arohabuchi@gmail.com>
2022-10-14 08:51:06 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - ommit some alphabet
+ *
+ * Return: Always (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
